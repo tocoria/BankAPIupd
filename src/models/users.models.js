ea3116e0caa44c92821a72faf039c08ed26265e0
Tomas <cauradev@gmail.com>
2022-10-07 01:50:32 +0200

    Schema updated & Postman environment set

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -3,13 +3,50 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
 
-    name: String,
-    lastName: String,
-    userName: String,
-    password: String,
-    email: String,
+    name: {
+        type: String,
+        required: [true, 'Name required']
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Lastname required']
+
+    },
+    userName: {
+        type: String,
+        required: [true, 'Username required']
+
+    },
+    password: {
+        type: String,
+        required: [true, 'Password required']
+
+    },
+    email: {
+        type: String,
+        required: [true, 'Email required']
+
+    },
     birthDate: Date,
-    accountNumber: Number
+    accountNumber: {
+        type: Number,
+        required: [true, 'Account number required']
+    },
+    role: {
+        type: String,
+        required: [true, 'Role has not been selected'],
+        enum: ['USER_ROLE', 'ADMIN_ROLE'],
+        default: 'USER_ROLE'
+    },
+    membershipType: {
+        type: String,
+        enum: ['SILVER', 'GOLD']
+    },
+    enabled: {
+        type: Boolean,
+        required: true,
+        default: true
+    }
 },
     {
     timestamps: true
